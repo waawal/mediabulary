0a1f9c6b279feb518b3f395556c0b6487b77abfa
Daniel Waardal <waawal@boom.ws>
2014-03-23 00:49:48 +0100

    adding noop web endpoint and Procfile for heroku

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+import gevent
+gevent.monkey.patch_all()
+import bottle
+
+app = bottle.Bottle()
+
+@app.get('/<query>')
+def process_query(query):
+	pass
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
