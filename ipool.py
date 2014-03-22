647ac91dea434945ded939d6c893447a7f1b18a9
Daniel Waardal <waawal@boom.ws>
2014-03-22 23:56:16 +0100

    adding api query for AS ipool

---
+++ ipool.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+import json
+import requests
+
+from htmllaundry import strip_markup
+
+
+BASE = 'http://ipool-extern.s.asideas.de:9090/api/v2/search'
+
+def query(self, q, limit=100, **kwargs):
+    """Get a lit of result from a query
+        query('katzen', 50)
+
+    """
+    payload = {'q': q, 'limit': limit}
+    if kwargs:
+        payload.extend(kwargs)
+    docs = requests.get(BASE, params=payload).json()['documents']
+    return [strip_markup(doc['content']) for doc in docs]
