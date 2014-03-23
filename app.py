3d2cbdf80b5fd95940ae9eead9a43fff5d9bf3b9
Daniel Waardal <waawal@boom.ws>
2014-03-23 01:43:13 +0100

    adding a test stub of data

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = bottle.Bottle()
 
 @app.get('/<query>')
 def process_query(query):
-	pass
+    return {
+        'Katze': 'Cat',
+        'Hund': 'Dog',
+        'Pferd': 'Horse'
+    }
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
