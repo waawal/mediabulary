25f98c56e09b6436b33116181e6cafd0b703a88b
Daniel Waardal <waawal@boom.ws>
2014-03-23 07:48:07 +0100

    calling our processing instead of serving dummy data

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from gevent import monkey
 monkey.patch_all()
 import bottle
 
+import .ipool
+import .langprocessing
+
 app = bottle.Bottle()
 
-@app.get('/<query>')
-def process_query(query):
-    return {
-        'Katze': 'Cat',
-        'Hund': 'Dog',
-        'Pferd': 'Horse'
-    }
+def process_query(q):
+    articles = ipool.query(q)
+    return langprocessing.process_documents(articles)
+
+
+@app.get('/flashcards/<query>')
+def index(query):
+    return dict(process_query(query))
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
