5ed73bc2175af8e074a283ca61cc174d083e3e00
Daniel Waardal <waawal@boom.ws>
2014-03-23 07:47:43 +0100

    refactoring structure

--- ipool.py
+++ ipool.py
@@ -2,8 +2,6 @@ import json
 import requests
 
 from htmllaundry import strip_markup
-from textblob import TextBlob
-from collections import Counter
 
 BASE = 'http://ipool-extern.s.asideas.de:9090/api/v2/search'
 
@@ -17,19 +15,3 @@ def query(self, q, limit=100, **kwargs):
         payload.extend(kwargs)
     docs = requests.get(BASE, params=payload).json()['documents']
     return [strip_markup(doc['content']) for doc in docs]
-
-def tokenize(data):
-    content = TextBlob(data[0])
-    return content.words
-
-def frequency(wordlist):
-    """ Takes a list of words/token and returns a dict
-        with the counts of the words/token.
-    """
-    return Counter(wordlist)
-
-if __name__ == "__main__":
-    data = query('katzen', 3)
-    wordlist = tokenize(data)
-    freq = frequency(wordlist)
-    print freq
--- langprocessing.py
+++ langprocessing.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from collections import Counter
 from pattern.de import parse, split
+from textblob import TextBlob
+from .stopwords import STOPWORDS
 
 def parse_text(text):
     """ takes german text, 1 or more sentences and applies part of speech information
@@ -18,9 +21,32 @@ def noun_list(sentence):
             nouns.append(the_word)
     return nouns
 
+def tokenize(data):
+    content = TextBlob(data)
+    return content.words
+
+def remove_stopwords(doc):
+    words = doc.split()
+    processed_words = [word for word in words if not word in STOPWORDS]
+    return ' '.join(processed_words)
+
+def find_most_frequent_words(docs, amount=20):
+    counter = Counter()
+    for doc in docs:
+        doc = remove_stopwords(doc)
+        wordlist = tokenize(doc)
+        counter.update(wordlist)
+    return counter.most_common(amount)
+
+def process_documents(docs, amount=20):
+    top_20 = find_most_frequent_words(docs, amount)
+    result = {}
+    for word in top_20:
+        result[word] = TextBlob(word).translate(from_lang="De", to="en")
+    return result
+
+
 if __name__ == "__main__":
-    # text = 'Auf der Krim haben russische Kräfte zwei weitere Militärbasen erobert.'
-    # text = 'Die Bayern hätten bereits an diesem Spieltag Meister werden können, darauf müssen sie jedoch noch ein bisschen warten.'
     text = "Auf der Suche nach seinen verschwundenen Eltern folgt der junge Peter Parker (Andrew Garfield) einer Spur, die ihn zu Wissenschaftler Curt Connors führt. In dessen Labor wird Peter von einer Spinne gebissen. Fortan verfügt er über Superkräfte, die er als Spider-Man im Kampf gegen das Verbrechen einsetzt. Doch als er es mit Connors' bösem Alter Ego \"The Lizard\" zu tun bekommt, steht Peter vor einer schweren Entscheidung. - Spider-Man ist zurück: Neustart der Marvel-Comicverfilmung, voll grandioser Effekte, Tempo und Action."
     parsed_text = parse_text(text)
     nouns = []
